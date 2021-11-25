77fda8be06a0720eb34c4259cdb38e99147c2d95
Achi Shengelia <achoshengelia@gmail.com>
2021-11-25 14:50:27 +0400

    Added Synth Gear to projects

--- src/components/ProjectsData.js
+++ src/components/ProjectsData.js
@@ -9,5 +9,16 @@ const projectsData = [
 		visit: 'https://github.com/RezikoChkadua/achi-backed',
 		id: 0,
 	},
+	{
+		title: 'Synth Gear',
+		description:
+			'My first full-stack application. Used express.js to create an app that follows the REST guidelines, Bootstrap for adding styles, EJS as a templating engine, MongoDB as the database and Mongoose as the Object Data Mapper. For authentication and authorisation I used passport.js. This project gives the users ability to create an account, add and delete (if they are authorised) items with the option to upload pictures (used Cloudinary for this). Also add and delete (if they are authorised) reviews. I used Moment.js to add a timer to each review and item to indicate how long ago it was created.',
+		image: '/gifs/synthgear.gif',
+		tags: ['Node', 'Express', 'MongoDB', 'Mongoose'],
+		source: 'https://synthgear.herokuapp.com/',
+		visit: 'https://github.com/achoshengelia/synth-gear',
+		id: 1,
+	},
 ];
+
 export default projectsData;
